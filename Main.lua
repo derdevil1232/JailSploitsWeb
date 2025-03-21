8039f70c156a4ad579794669d70f038dcbd91140
derdevil1232 <joeeggb@gmail.com>
2025-03-21 18:57:26 +0000

    Main.lua

--- Main.lua
+++ Main.lua
@@ -1,20 +1,10 @@
-if not LPH_OBFUSCATED then
-getfenv().LPH_JIT_MAX          = function(...) return ... end;
-getfenv().LPH_NO_VIRTUALIZE    = function(...) return ... end;
-getfenv().LPH_ENCSTR           = function(...) return ... end;
-end
 
-if getgenv().Jailhax then
-    return
-else
-    getgenv().Jailhax = true
-end
+
 --> [[ Load Game ]] <--
 
 while not game:IsLoaded() do
     task.wait()
 end
-
 
 
 --> [[ Load Services ]] <--
@@ -189,9 +179,8 @@ local Teleport = LPH_JIT_MAX(function(location)
     end)
 end)
 
---> [[ Load Library ]] <--
 
-local Library = loadstring(game:HttpGet("https://jailhax.lol/misc/Gui2.lua"))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 do
     local Window                   = Library:CreateWindow({
@@ -204,138 +193,79 @@ do
 		MinimizeKey                = Enum.KeyCode.RightControl 
     })            
 
-    local Tabs                     = {
-		Player                     = Window:AddTab({ Title = "Main", Icon = "" }),
-		Robberies                  = Window:AddTab({ Title = "Robberies", Icon = "" }),
-    }
-
-    local Options                  = Library.Options
-
-    do
-		local AntiTazeToggle       = Tabs.Main:AddToggle("Antitaze", { 
-            Title = "Anti Taze", 
-            Default = true 
-        })
-
-        local AntiArrestToggle     = Tabs.Main:AddToggle("Antiarrest", { 
-            Title = "Anti Arrest", 
-            Default = true 
-        })
-
-		local NoRagDollToggle      = Tabs.Main:AddToggle("Noragdoll", { 
-            Title = "No Rag Doll", 
-            Default = true 
-        })
-
-		local NoFallDamageToggle   = Tabs.Main:AddToggle("Nofalldamage", { 
-            Title = "No Fall Damage", 
-            Default = true 
-        })
-
-		local NoParachuteToggle    = Tabs.Main:AddToggle("Noparachute", { 
-            Title = "No Parachute", 
-            Default = true 
-        })
 
 
+    local Window = Rayfield:CreateWindow({
+        Name = "HypeSpeed V1 - discord.gg/ehpR5vZr42",
+        Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+        LoadingTitle = "HypeSpeed V1",
+        LoadingSubtitle = "by C3NIOR",
+        Theme = "AmberGlow", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+     
+        DisableRayfieldPrompts = false,
+        DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+     
+        ConfigurationSaving = {
+           Enabled = true,
+           FolderName = nil, -- Create a custom folder for your hub/game
+           FileName = "HypeSpeed"
+        },
+     
+        Discord = {
+           Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+           Invite = "ehpR5vZr42", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+           RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+        },
+     
+        KeySystem = false, -- Set this to true to use our key system
+        KeySettings = {
+           Title = "No Key System",
+           Subtitle = "Key System",
+           Note = "No Key system!", -- Use this to tell the user how to get a key
+           FileName = "ehpR5vZr42", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+           SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+           GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+           Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+        }
+     })
 
-		AntiTazeToggle:OnChanged(function()
-			Settings.Antitaze = Options.Antitaze.Value
-		end)
-
-        AntiArrestToggle:OnChanged(function()
-			Settings.Antiarrest = Options.Antiarrest.Value
-
-            if Options.Antiarrest.Value == false then
-                pcall(function()
-                    Humanoid:SetStateEnabled(Enum.HumanoidStateType.Seated, true)
-                    Humanoid.Sit = false
-                end)
-            end
-		end)
 
 
-		NoRagDollToggle:OnChanged(function()
-			Settings.NoRagDoll = Options.Noragdoll.Value
-		end)
 
-		NoFallDamageToggle:OnChanged(function()
-			Settings.NoFallDamage = Options.Nofalldamage.Value
-		end)
 
-		NoParachuteToggle:OnChanged(function()
-			Settings.NoParachute = Options.Noparachute.Value
-		end)
     
+     local Tab = Window:CreateTab("Main", 4483362458) -- Title, Image
 
-
-
-    Tabs.Robberies:AddButton({
-        Title = "Disable Mansion Lasers",
-        Description = "Disables Mansion Lasers.",
-        Callback = function()
-            for _, v in Workspace.MansionRobbery:GetDescendants() do
-                if v.Name == "LaserTraps" and v:IsA("Model") then
-                    for _, v2 in v:GetDescendants() do
-                        if v2:FindFirstChild("TouchInterest") then
-                            v2:Destroy()
-                        end
-                    end
-                end
-
-                if v.Name == "Lasers" and v:IsA("Model") then 
-                    for _, v2 in v:GetDescendants() do
-                        if v2:FindFirstChild("TouchInterest") then
-                            v2:Destroy()
-                        end
-                    end
+local Toggle = Main:CreateToggle({
+    Name = "Enabled",
+    CurrentValue = true,
+    Flag = "8snb40cmsicy", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function( for _, v in Workspace.MansionRobbery:GetDescendants() do
+        if v.Name == "LaserTraps" and v:IsA("Model") then
+            for _, v2 in v:GetDescendants() do
+                if v2:FindFirstChild("TouchInterest") then
+                    v2:Destroy()
                 end
             end
-        end,
-    })
-
-
-
-    Tabs.Teleports:AddButton({
-        Title = "Mansion",
-        Callback = function()
-            Teleport("Mansion")
         end
-    })
-
-    local AimbotToggle = Tabs.Weapons:AddToggle("Aimbot", {
-        Title = "Aimbot",
-        Default = true
-    })
-
-    local PopTiresToggle = Tabs.Weapons:AddToggle("Poptires", {
-        Title = "Pop Tires",
-        Default = true
-    })
-
-    AimbotToggle:OnChanged(function()
-        Settings.Aimbot = Library.Options.Aimbot.Value
-    end)
-
-    PopTiresToggle:OnChanged(function()
-        Settings.Poptires = Library.Options.Poptires.Value
-    end)
-
-    Tabs.Weapons:AddSlider("Aimbotdistance", {
-        Title = "Aimbot Distance",
-        Default = 100,
-        Min = 100,
-        Max = 1000,
-        Rounding = 1,
-        Callback = function(Value)
-            Settings.AimbotDistanceInt = Value
+        if v.Name == "Lasers" and v:IsA("Model") then 
+            for _, v2 in v:GetDescendants() do
+                if v2:FindFirstChild("TouchInterest") then
+                    v2:Destroy()
+                end
+            end
         end
-    })
+    end)
+    -- The function that takes place when the toggle is pressed
+    -- The variable (Value) is a boolean on whether the toggle is true or false
+    end,
+ })
 
-    Window:SelectTab(1)
+
+
+
 end
 
---> [[ Load Main ]] <--
 
 local FakeSniper = {
     Local = true,
@@ -433,14 +363,4 @@ Spawn(function()
         end
     end)()
 end)
-
-
-UserInputService.InputBegan:Connect(LPH_NO_VIRTUALIZE(function(input, event)
-
-    if input.UserInputType == Enum.UserInputType.Keyboard then
-        if input.KeyCode == Enum.KeyCode.LeftControl or input.KeyCode == Enum.KeyCode.RightControl then
-            RollTimeFunction()
-        end
-    end
-end))
 
